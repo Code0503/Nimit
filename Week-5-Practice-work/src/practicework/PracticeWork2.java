b6fa6f027514ce4cde30ae2453c9607e8e5b145c
code0503 <nimitpatel285@gmail.com>
2023-04-28 17:42:47 +0100

    practicework

---
+++ Week-5-Practice-work/src/practicework/PracticeWork2.java
@@ -0,0 +1,16 @@
+package practicework ;
+
+public class PracticeWork2 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        boolean c = a == b;
+
+        System.out.println(c);
+    }
+
+}
+
+
